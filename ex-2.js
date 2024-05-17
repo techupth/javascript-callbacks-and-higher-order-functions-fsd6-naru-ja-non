42f3466f9146cc11c88844a898fb901a29ddc8d9
Athikhom_ra <athikhom_ra@outlook.com>
2024-05-17 12:33:53 +0700

    Assignment#1-2 'callback function' NARu

--- ex-2.js
+++ ex-2.js
@@ -2,6 +2,20 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  if (operation(array) >= 5) {
+    return "ผ่านเกณฑ์ ✅";
+  } else {
+    return "ไม่ผ่านเกณฑ์ ❌";
+  }
+}
+
+function serveyPoint(array) {
+  let count = 0;
+  for (let score of array) {
+    // console.log(score);
+    score >= 70 ? count++ : (count = count);
+  }
+  return count;
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
@@ -10,6 +24,18 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+let scoreRoom1Result = `นักเรียนห้องที่ 1 ${atLeastFive(
+  studentScoresRoom1,
+  serveyPoint
+)}`;
+let scoreRoom2Result = `นักเรียนห้องที่ 2 ${atLeastFive(
+  studentScoresRoom2,
+  serveyPoint
+)}`;
+let scoreRoom3Result = `นักเรียนห้องที่ 3 ${atLeastFive(
+  studentScoresRoom3,
+  serveyPoint
+)}`;
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
